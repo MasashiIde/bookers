0c88b6217c51d4d466329996bc2ea3a5db9fea41
MasashiIde <117148113+MasashiIde@users.noreply.github.com>
2022-11-09 12:16:18 +0000

    [Add]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
   def create
     book= Book.new(book_params)
     book.save
-    redirect_to '/books/#{book.id}'
+    flash[:created]= "Book was successfully created."
+    redirect_to books_path
   end
 
   def index
@@ -19,6 +20,20 @@ class BooksController < ApplicationController
 
   def edit
     @book= Book.find(params[:id])
+  end
+
+  def update
+    book= Book.find(params[:id])
+    book.update(book_params)
+    flash[:updated]= "Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book= Book.find(params[:id])
+    book.destroy
+    flash[:destroyed]= "Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
